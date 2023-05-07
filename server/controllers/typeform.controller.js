31e6870bc18efd730532776ac9a664d43be88d06
Pollock Nag <75268620+Pollock-Nag@users.noreply.github.com>
2023-05-07 18:58:32 +0600

    change fromResponse controller and model

--- server/controllers/typeform.controller.js
+++ server/controllers/typeform.controller.js
@@ -1,19 +1,19 @@
 const FormResponse = require('../models/typeform/typeform.model');
-const postTypeform = async (req, res) => {
-  //   console.log('typeform', req.body);
-  const formResponse = req.body;
-  //   console.log(formResponse);
+const postTypeform = (req, res) => {
+  const formResponse = req.body.form_response;
+  console.log(formResponse);
 
   const newFormResponse = new FormResponse({
     form_id: formResponse.form_id,
     submitted_at: formResponse.submitted_at,
-    name: formResponse.answers[0].text,
-    email: formResponse.answers[2].email,
-    phone: formResponse.answers[3].phone_number,
+    answers: formResponse.answers.map((answer) => ({
+      field: answer.field.id,
+      answer: answer.text,
+    })),
   });
 
   newFormResponse.save();
-  res.status(200).send('submitted successfully');
+  res.send();
 };
 
 module.exports = {
--- server/models/typeform/typeform.model.js
+++ server/models/typeform/typeform.model.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose');
 const formResponseSchema = new mongoose.Schema({
   form_id: String,
   submitted_at: Date,
-  name: String,
-  email: String,
-  phone: String,
+  answers: [
+    {
+      field: String,
+      answer: String,
+    },
+  ],
 });
 const FormResponse = mongoose.model('FormResponse', formResponseSchema);
 module.exports = FormResponse;
