8b01031e189b5980acfe9a612a15a6b3bad7a2e0
Pollock Nag <75268620+Pollock-Nag@users.noreply.github.com>
2023-05-11 19:38:13 +0600

    added alumni model

---
+++ server/models/alumni/alumni.model.js
@@ -0,0 +1,96 @@
+const mongoose = require('mongoose');
+const softTechSkill = require('../student/schemas/softTechSkills.schema');
+
+const alumniSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  about: {
+    type: String,
+  },
+  education: [
+    {
+      instituteName: {
+        type: String,
+      },
+      program: {
+        type: String,
+      },
+      fromDate: {
+        type: Date,
+      },
+      toDate: {
+        type: Date,
+      },
+      description: {
+        type: String,
+      },
+      status: {
+        type: Boolean,
+      },
+      gpa: {
+        type: Number,
+      },
+    },
+  ],
+  experiences: [
+    {
+      jobTitle: {
+        type: String,
+      },
+      companyName: {
+        type: String,
+      },
+      fromDate: {
+        type: Date,
+      },
+      toDate: {
+        type: Date,
+      },
+      description: {
+        type: String,
+      },
+      skills: [String],
+      status: {
+        type: Boolean,
+      },
+    },
+  ],
+  skills: [softTechSkill],
+  certifications: [
+    {
+      name: {
+        type: String,
+      },
+      url: {
+        type: String,
+      },
+    },
+  ],
+  onlineJudge: [
+    {
+      name: {
+        type: String,
+      },
+      username: {
+        type: String,
+      },
+      isVerified: {
+        type: Boolean,
+      },
+    },
+  ],
+  projects: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Project',
+    },
+  ],
+  socialLinks: [String],
+});
+const Alumni = mongoose.model('User', alumniSchema);
+module.exports = Alumni;
