8e4a019caf891d5fbbbf607538b160cca46b7752
saimon siddique <saimonsiddiquee@gmail.com>
2023-05-11 14:37:50 +0600

    Added sidebar, smallNameCard

---
+++ client/src/components/alumniComponents/SmallNameCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SmallNameCard = () => {
+  return (
+    <div className="bordered shadow-lg w-60 h-14 flex bg-gray-100 p-2 rounded-md">
+      <div className="avatar">
+        <div className="w-12 rounded-xl">
+          <img src="https://raw.githubusercontent.com/zahidtwt/zahidlive/main/277801721_1146010236235641_4251157026316733609_n.jpg" />
+        </div>
+      </div>
+      <div className="flex flex-col justify-center ml-2">
+        <div className="font-semibold">Md Zahid Hossain</div>
+        <div className="text-xs text-gray-500">Alumni</div>
+      </div>
+    </div>
+  );
+};
+
+export default SmallNameCard;
